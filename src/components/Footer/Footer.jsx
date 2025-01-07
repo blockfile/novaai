f670cc5c0fcdb2002485b0f2229ab71ef2b1e0a5
renzivan243 <virusbsc.dev00@gmail.com>
2025-01-07 17:33:35 +0800

    nova

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,32 +4,32 @@ import { BsGlobe } from "react-icons/bs";
 
 function Footer() {
     return (
-        <div className="flex flex-col justify-center items-center  text-white text-center ">
-            <div className=" bottom-10 left-0 right-0 flex justify-center items-center space-x-7 ">
+        <div className="flex flex-col justify-center items-center text-white text-center mt-auto">
+            <div className="bottom-10 left-0 right-0 flex justify-center items-center space-x-7">
                 <a
-                    href="https://t.me/VirusQuarantinePortal"
+                    href="https://t.me/NovaA_Portal"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-2xl">
                     <FaTelegram />
                 </a>
                 <a
-                    href="https://twitter.com/virus020324"
+                    href="https://x.com/NovaAI_x"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-2xl">
                     <FaTwitter />
                 </a>
                 <a
-                    href="https://infectedbyvirus.com/"
+                    href="https://novaai.systems/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-2xl">
                     <BsGlobe />
                 </a>
             </div>
-            <div className=" bottom-2 left-0 right-0 mt-5  text-xs">
-                ©2024 Virus | All Rights Reserved
+            <div className="bottom-2 left-0 right-0 mt-5 text-xs">
+                ©2025 NOVA AI | All Rights Reserved
             </div>
         </div>
     );
